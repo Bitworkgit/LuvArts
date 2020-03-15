2be06f201885e03eb32553eaa9cd42834a4076be
Thiago Piassi Bonfogo <thiagoprofissional29@gmail.com>
2020-03-15 20:47:18 -0300

    Implantação da pagina de login

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+import 'popper.js';
+import 'bootstrap';
+import 'select2';
+import 'perfect-scrollbar';
+import 'daterangepicker';
+import 'countdowntime';
+import 'animsition';
+
---
+++ src/resources/views/user/login/login.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login V9</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+	<link rel="stylesheet" type="text/css" href="{{ asset("/pages/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css")}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset("/pages/login/fonts/iconic/css/material-design-iconic-font.min.css")}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset("/pages/login/animate.css") }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset("/css/app.css") }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset("/pages/login/css/util.css") }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset("/pages/login/css/main.css") }}">
+</head>
+<body>
+	
+	
+	<div class="container-login100" >
+		<div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
+			<form class="login100-form validate-form">
+				<span class="login100-form-title p-b-37">
+					Sign In
+				</span>
+
+				<div class="wrap-input100 validate-input m-b-20" data-validate="Enter username or email">
+					<input class="input100" type="text" name="username" placeholder="username or email">
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="wrap-input100 validate-input m-b-25" data-validate = "Enter password">
+					<input class="input100" type="password" name="pass" placeholder="password">
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="container-login100-form-btn">
+					<button class="login100-form-btn">
+						Sign In
+					</button>
+				</div>
+
+				<div class="text-center p-t-57 p-b-20">
+					<span class="txt1">
+						Or login with
+					</span>
+				</div>
+
+				<div class="flex-c p-b-112">
+					<a href="#" class="login100-social-item">
+						<i class="fa fa-facebook-f"></i>
+					</a>
+
+					<a href="#" class="login100-social-item">
+						<img src="{{ asset("/pages/login/images/icons/icon-google.png")}}" alt="GOOGLE">
+					</a>
+				</div>
+
+				<div class="text-center">
+					<a href="#" class="txt2 hov1">
+						Sign Up
+					</a>
+				</div>
+			</form>
+
+			
+		</div>
+	</div>
+	
+	<div id="dropDownSelect1"></div>
+	<script src="https://code.jquery.com/jquery-3.4.1.js"integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="{{ asset("js/app.js") }}"></script>
+	<script src="{{ asset("/pages/login/js/main.js")}}"></script>
+
+</body>
+</html>
