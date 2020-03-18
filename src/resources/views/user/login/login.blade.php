be3ea0d2db2d8db407b1de7a63c0924cde64522d
Higorgx <higorg_freitas@hotmail.com>
2020-03-18 13:22:16 -0300

    brabo memo o login ta show

--- src/resources/views/user/login/login.blade.php
+++ src/resources/views/user/login/login.blade.php
@@ -16,7 +16,7 @@
 <body>
 	
 	
-	<div class="container-login100" >
+	<div class="container-login100" style="background-image: url('/pages/login/images/bg-01.jpg');">
 		<div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
 			<form class="login100-form validate-form">
 				<span class="login100-form-title p-b-37">
