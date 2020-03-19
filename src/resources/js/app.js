ff37ac0b07d87f1d8775ce1a43354918cf7b8fe9
Thiago Piassi Bonfogo <thiagoprofissional29@gmail.com>
2020-03-19 11:42:52 -0300

    reconstrução de dependências
    1/7

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,9 +1,9 @@
 require('./bootstrap');
-import 'popper.js';
-import 'bootstrap';
-import 'select2';
-import 'perfect-scrollbar';
-import 'daterangepicker';
-import 'countdowntime';
-import 'animsition';
+// import 'popper.js';
+// import 'bootstrap';
+// import 'select2';
+// import 'perfect-scrollbar';
+// import 'daterangepicker';
+// import 'countdowntime';
+//import 'animsition';
 
