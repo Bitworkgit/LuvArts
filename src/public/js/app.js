2b711c6cb881656b4a40576be4a9f6b3a00e5cd0
Thiago Piassi Bonfogo <53578441+thiagopiassigit@users.noreply.github.com>
2020-04-07 22:49:28 -0300

    ...

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -66504,8 +66504,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\higor\Área de Trabalho\PA\a\src\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\higor\Área de Trabalho\PA\a\src\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\thiag\Documents\GitHub\LuvArts\src\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\thiag\Documents\GitHub\LuvArts\src\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
