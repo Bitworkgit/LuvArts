f33b6c5f787c505d1965e971e3a8c9bf176176a2
Bruno César <>
2020-04-08 14:28:32 -0300

    Envio do update.php

--- src/update.php
+++ src/update.php
@@ -2,4 +2,8 @@
     system("php artisan migrate"); 
     system("npm install");
     system("npm run dev");
+    system("composer update");
+    system("php artisan db:seed");
+    system("php artisan migrate");
+    
 ?>
