98aec9e4c2ff4120b1ab1cab3304076d1a52c90c
Thiago Piassi Bonfogo <thiagoprofissional29@gmail.com>
2020-05-03 11:19:20 -0300

    criação de mais um componente de gráfico

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -28,6 +28,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('grafico-linha', require('./components/GraficoDeLinha.vue').default);
+Vue.component('grafico-pizza', require('./components/GraficoDePizza.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- src/resources/views/admin/luvArts-capital.blade.php
+++ src/resources/views/admin/luvArts-capital.blade.php
@@ -2,32 +2,21 @@
 
 @section('title', 'Gerenciador de Usuários')
 
+@section('css')
+    <link rel="stylesheet" href="{{ asset('pages/admin/equipeCapital.css') }}">
+@endsection
+
 @section('content_header')
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <!-- <div id="linechart" style="width: 900px; height: 500px"></div> -->
     <script>
-        var dados = <?php echo $dados; ?>
-        dadosDivisao = <?php echo $dadosDivisao; ?>
-
-        function pizzaChart() {
-
-            var data = google.visualization.arrayToDataTable(dadosDivisao);
-
-            var options = {
-            title: 'Divisão do capital'
-            };
-
-            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-            chart.draw(data, options);
-        }
+        var dados = <?php echo $dados; ?>;
+        var dadosDivisao = <?php echo $dadosDivisao; ?>;
     </script>
 
     <div id="app">
         <grafico-linha></grafico-linha>
+        <grafico-pizza></grafico-pizza>
     </div>
-
-    <div id="piechart" style="width: 900px; height: 500px;"></div>
     <br>
     Capital total: R$ {{ $capital }}<br>
     Crescimento da empresa: {{ number_format($crescimento,2) }}%
