49e747f93c9f41713318d27134263b1b37f3fd04
Thiago Piassi Bonfogo <thiagoprofissional29@gmail.com>
2020-05-06 23:42:29 -0300

    ...

--- src/database/migrations/2020_05_03_030741_create_saldo_equipes_table.php
+++ src/database/migrations/2020_05_03_030741_create_saldo_equipes_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Model\SaldoEquipe;
 
 class CreateSaldoEquipesTable extends Migration
 {
