53fdd8afb38f053e66d74d64289a2a50cf1aadd4
Thiago Piassi Bonfogo <thiagoprofissional29@gmail.com>
2020-05-09 13:16:46 -0300

    ...

--- src/resources/views/produtos/pesquisa.blade.php
+++ src/resources/views/produtos/pesquisa.blade.php
@@ -7,56 +7,169 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>pesquisando por {{$pesquisa}}</title>
   <link rel="stylesheet" type="text/css" href="{{ asset("/css/app.css") }}">
+  <link rel="stylesheet" href="{{ asset('pages/home/style.css') }}">
+
 
 </head>
 
 <body>
-  <a class="" href="#" style="
-    /* margin-top: 77px; */
-    z-index:  99999;
-    padding: 10px;
-    position: absolute;
-"><img src="img\NAV.png" width="140" class="nav-img  "></a>
-  <nav class="navbar navbar-expand-lg navbar-dark ">
-    <a class="navbar-brand" href="#"><img src="img\" width="110" class="nav-link nav-img "></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <div class="col-md-8">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
+  <a class="" href="{{ route('home') }}" style="
+  /* margin-top: 77px; */
+  z-index:  99999;
+  padding: 10px;
+  position: absolute;
+"><img src="{{ asset('images/NAV.png')}}" width="140" class="nav-img  "></a>
+<nav class="navbar navbar-expand-lg navbar-dark ">
+
+  <a class="navbar-brand" href="{{ route('home') }}"><img src="http://127.0.0.1:8000/images/dark.png" height="10px" width="140" class="nav-img  "></a>
+
+  
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="col-md-8">
+      <ul class="navbar-nav mr-auto">
+@if(Auth::check())
+          <?php $categorias = App\Model\Categoria::all() ?>
+          <?php $carrinho = App\Carrinho::where('comprador_id',Auth::User()->id)->get(); ?>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+              <a class="nav-link menu dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
               aria-haspopup="true" aria-expanded="false">
-              Dropdown
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
+              <img src="{{ asset('images/cart.png')}}" width="30" class="nav-img  ">
+              
+              </a>
+              <div class="dropdown-menu cart" aria-labelledby="navbarDropdown">
+              
+              <table class="table table-hover">
+              <thead>
+                  <tr>
+                      <th>Produto</th>
+                      <th></th>
+                      <th class="text-center"></th>
+                      <th class="text-center">Preço</th>
+                      <th> </th>
+                  </tr>
+              </thead>
+              <tbody>
+                  @foreach($carrinho as $item)
+                      <tr>
+                          <td class="col-sm-8 col-md-6">
+                          <div class="media">
+                              <img class="media-object" src="{{$item->produto()->get()->first()->user_id == 1 ? asset($item->produto()->get()->first()->ende_foto_pro) : Storage::url($item->produto()->get()->first()->ende_foto_pro)}}" style="width: 72px; height: 72px;">
+                              <div class="media-body">
+                                  <h4 class="media-heading"><a href="{{route('produto.comprar',['id' => $item->produto()->get()->first()->id])}}">{{$item->produto()->get()->first()->nome_pro}}</a></h4>
+                              </div>
+                          </div></td>
+                          <td class="col-sm-1 col-md-1" style="text-align: center">
+                          </td>
+                          <td class="col-sm-1 col-md-1 text-center"><strong></strong></td>
+                          <td class="col-sm-1 col-md-1 text-center"><strong>R$ {{$item->produto()->get()->first()->preco_pro}}</strong></td>
+                          <td class="col-sm-1 col-md-1">
+                              <a style="color: black; text-decoration: none;" href="{{route('produto.removerDoCarrinho',['id' => $item->id])}}">
+                                  <button type="button" class="btn btn-danger">
+                                      <span class="glyphicon glyphicon-remove"></span>
+                                          Remover
+                                      
+                                  </button>
+                              </a>
+                          </td>
+                      </tr>
+                  @endforeach
+                  
+                  <tr>
+                      <td>   </td>
+                      <td>   </td>
+                      <td>   </td>
+                      <td>
+                  </td>
+                      <td>
+                      <button type="button" class="btn btn-dark " style="background-color: #414251!important;">
+                          Comprar <span class="glyphicon glyphicon-play"></span>
+                      </button></td>
+                  </tr>
+              </tbody>
+          </table>
+
+
+
+              
+              </div>
+          </li>
+      @endif
+
+
+
+        <li class="nav-item active">
+          <a class="nav-link menu" href="{{ route('home') }}">Início <span class="sr-only">(current)</span></a>
+        </li>
+    
+        <li class="nav-item">
+          <a class="nav-link menu" href="#">Sobre nós</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link menu dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Usuário
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            @if(!Auth::check())
+            <a class="dropdown-item" href="{{ route('user.login') }}">Entrar</a>
+            <a class="dropdown-item" href="{{ route('user.register') }}">Cadastrar-se</a>
               <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"><br></a>
-          </li>
-        </ul>
-      </div>
-      <div class="col-md-4">
-        <div class="Navcont">
-          <input type="text" placeholder="Search...">
-          <div class="navsearch"></div>
-        </div>
-      </div>
+            @endif
+            @if(Auth::check())
+              <a class="dropdown-item" href="{{ route('profile.index',['user_id' => Auth::User()->id]) }}">Meu Perfil</a>
+              <a class="dropdown-item" href="{{ route('user.sair') }}">Sair</a>
+            @endif
+          </div>
+        </li>
+  <!----------------------------------------------------------------------------------------------->
+     <li class="nav-item dropdown">
+          <a class="nav-link menu dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Categorias
+          </a>
+          <div class="dropdown-menu scrollable-menu" aria-labelledby="navbarDropdown">
+              @foreach($categorias as $categoria)
+                  <a class="dropdown-item" href="{{ route('pesquisar.categoria', ['categoria_id' => $categoria->id]) }}">{{ $categoria->nome_categoria }}</a>
+              @endforeach
+          </div>
+        </li>
+  
+
+  <!------------------------------------------------------------------------------------------------------->
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"><br></a>
+        </li>
+      </ul>
     </div>
-  </nav>
+
+
+
+    <form method="GET" action="{{ route('pesquisar') }}">
+      <div class="col-md-4">
+          <div class="Navcont">
+          <input type="text" id="pesquisa" placeholder="Pesquisar...">
+          <div class="navsearch"></div>
+          </div>
+      </div>
+    </form>
+  </div>
+</nav>
+<script>
+    var entrada = document.getElementById('pesquisa');
+    entrada.addEventListener("keyup", function(event) {
+      if (event.keyCode === 13) {
+          var url = '{{ route('pesquisar') }}/' + $("#pesquisa").val();
+          window.open(url,"_blank");
+      }
+    });
+
+</script>
+
 
   <div class="container">
 
--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -17,115 +17,162 @@
   </style>
   </head>
 <body>
- <a class="" href="{{ route('home') }}" style="
-        /* margin-top: 77px; */
-        z-index:  99999;
-        padding: 10px;
-        position: absolute;
-    "><img src="{{ asset('images/NAV.png')}}" width="140" class="nav-img  "></a>
-      <nav class="navbar navbar-expand-lg navbar-dark ">
+  <a class="" href="{{ route('home') }}" style="
+  /* margin-top: 77px; */
+  z-index:  99999;
+  padding: 10px;
+  position: absolute;
+"><img src="{{ asset('images/NAV.png')}}" width="140" class="nav-img  "></a>
+<nav class="navbar navbar-expand-lg navbar-dark ">
 
-        <a class="navbar-brand" href="{{ route('home') }}"><img src="http://127.0.0.1:8000/images/dark.png" height="10px" width="140" class="nav-img  "></a>
+  <a class="navbar-brand" href="{{ route('home') }}"><img src="http://127.0.0.1:8000/images/dark.png" height="10px" width="140" class="nav-img  "></a>
 
-        
+  
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <div class="col-md-8">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link menu" href="{{ route('home') }}">Início <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link menu" href="#">Sobre nós</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link menu dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-                  aria-haspopup="true" aria-expanded="false">
-                  Usuário
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  @if(!Auth::check())
-                  <a class="dropdown-item" href="{{ route('user.login') }}">Entrar</a>
-                  <a class="dropdown-item" href="{{ route('user.register') }}">Cadastrar-se</a>
-                    <div class="dropdown-divider"></div>
-                  @endif
-                  @if(Auth::check())
-                    <a class="dropdown-item" href="http://127.0.0.1:8000/profile/6">Meu Perfil</a>
-                    <a class="dropdown-item" href="{{ route('user.sair') }}">Sair</a>
-                  @endif
-                </div>
-              </li>
-			  <!----------------------------------------------------------------------------------------------->
-			     <li class="nav-item dropdown">
-                <a class="nav-link menu dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-                  aria-haspopup="true" aria-expanded="false">
-                  Categorias
-                </a>
-                <div class="dropdown-menu scrollable-menu" aria-labelledby="navbarDropdown">
-                 
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="col-md-8">
+      <ul class="navbar-nav mr-auto">
+@if(Auth::check())
+          <?php $categorias = App\Model\Categoria::all() ?>
+          <?php $carrinho = App\Carrinho::where('comprador_id',Auth::User()->id)->get(); ?>
+          <li class="nav-item dropdown">
+              <a class="nav-link menu dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              <img src="{{ asset('images/cart.png')}}" width="30" class="nav-img  ">
+              
+              </a>
+              <div class="dropdown-menu cart" aria-labelledby="navbarDropdown">
+              
+              <table class="table table-hover">
+              <thead>
+                  <tr>
+                      <th>Produto</th>
+                      <th></th>
+                      <th class="text-center"></th>
+                      <th class="text-center">Preço</th>
+                      <th> </th>
+                  </tr>
+              </thead>
+              <tbody>
+                  @foreach($carrinho as $item)
+                      <tr>
+                          <td class="col-sm-8 col-md-6">
+                          <div class="media">
+                              <img class="media-object" src="{{$item->produto()->get()->first()->user_id == 1 ? asset($item->produto()->get()->first()->ende_foto_pro) : Storage::url($item->produto()->get()->first()->ende_foto_pro)}}" style="width: 72px; height: 72px;">
+                              <div class="media-body">
+                                  <h4 class="media-heading"><a href="{{route('produto.comprar',['id' => $item->produto()->get()->first()->id])}}">{{$item->produto()->get()->first()->nome_pro}}</a></h4>
+                              </div>
+                          </div></td>
+                          <td class="col-sm-1 col-md-1" style="text-align: center">
+                          </td>
+                          <td class="col-sm-1 col-md-1 text-center"><strong></strong></td>
+                          <td class="col-sm-1 col-md-1 text-center"><strong>R$ {{$item->produto()->get()->first()->preco_pro}}</strong></td>
+                          <td class="col-sm-1 col-md-1">
+                              <a style="color: black; text-decoration: none;" href="{{route('produto.removerDoCarrinho',['id' => $item->id])}}">
+                                  <button type="button" class="btn btn-danger">
+                                      <span class="glyphicon glyphicon-remove"></span>
+                                          Remover
+                                      
+                                  </button>
+                              </a>
+                          </td>
+                      </tr>
+                  @endforeach
                   
-                  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
-				  <a class="dropdown-item" href="">Cadastrar-se</a>
+                  <tr>
+                      <td>   </td>
+                      <td>   </td>
+                      <td>   </td>
+                      <td>
+                  </td>
+                      <td>
+                      <button type="button" class="btn btn-dark " style="background-color: #414251!important;">
+                          Comprar <span class="glyphicon glyphicon-play"></span>
+                      </button></td>
+                  </tr>
+              </tbody>
+          </table>
 
 
-                  
-                </div>
-              </li>
-			  <!------------------------------------------------------------------------------------------------------->
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"><br></a>
-              </li>
-            </ul>
+
+              
+              </div>
+          </li>
+      @endif
+
+
+
+        <li class="nav-item active">
+          <a class="nav-link menu" href="{{ route('home') }}">Início <span class="sr-only">(current)</span></a>
+        </li>
+    
+        <li class="nav-item">
+          <a class="nav-link menu" href="#">Sobre nós</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link menu dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Usuário
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            @if(!Auth::check())
+            <a class="dropdown-item" href="{{ route('user.login') }}">Entrar</a>
+            <a class="dropdown-item" href="{{ route('user.register') }}">Cadastrar-se</a>
+              <div class="dropdown-divider"></div>
+            @endif
+            @if(Auth::check())
+              <a class="dropdown-item" href="{{ route('profile.index',['user_id' => Auth::User()->id]) }}">Meu Perfil</a>
+              <a class="dropdown-item" href="{{ route('user.sair') }}">Sair</a>
+            @endif
           </div>
-          <div class="col-md-4">
-            <div class="Navcont">
-              <input type="text" placeholder="Pesquisar...">
-              <div class="navsearch"></div>
-            </div>
+        </li>
+  <!----------------------------------------------------------------------------------------------->
+     <li class="nav-item dropdown">
+          <a class="nav-link menu dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Categorias
+          </a>
+          <div class="dropdown-menu scrollable-menu" aria-labelledby="navbarDropdown">
+              @foreach($categorias as $categoria)
+                  <a class="dropdown-item" href="{{ route('pesquisar.categoria', ['categoria_id' => $categoria->id]) }}">{{ $categoria->nome_categoria }}</a>
+              @endforeach
           </div>
-        </div>
-      </nav>
+        </li>
+  
 
+  <!------------------------------------------------------------------------------------------------------->
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"><br></a>
+        </li>
+      </ul>
+    </div>
+
+
+
+    <form method="GET" action="{{ route('pesquisar') }}">
+      <div class="col-md-4">
+          <div class="Navcont">
+          <input type="text" id="pesquisa" placeholder="Pesquisar...">
+          <div class="navsearch"></div>
+          </div>
+      </div>
+    </form>
+  </div>
+</nav>
+<script>
+    var entrada = document.getElementById('pesquisa');
+    entrada.addEventListener("keyup", function(event) {
+      if (event.keyCode === 13) {
+          var url = '{{ route('pesquisar') }}/' + $("#pesquisa").val();
+          window.open(url,"_blank");
+      }
+    });
+
+</script>
   <div class="row">
     <div class="col-md-12 ">
       <p align="center" class="titulo">Top Mais Vendidos</p>
