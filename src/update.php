7d453ca1f55b7c98924ec988bd600db7edf2ec52
Thiago Piassi Bonfogo <thiagoprofissional29@gmail.com>
2020-05-17 11:56:49 -0300

    Update update.php

--- src/update.php
+++ src/update.php
@@ -4,7 +4,7 @@
     system("npm run dev");
     system("composer update");
     system("php artisan db:seed");
-    system("php artisan serve");
     system('composer require jeroennoten/laravel-adminlte');
     system('php artisan adminlte:install');
+    system("php artisan serve");
 ?>
