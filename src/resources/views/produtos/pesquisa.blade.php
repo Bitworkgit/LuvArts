46688db0f2890dc95e7bc77f491b5de8074f7246
Higorgx <36903838+Higorgx@users.noreply.github.com>
2020-05-17 14:41:31 -0300

    FINAL MEU

--- src/resources/views/produtos/pesquisa.blade.php
+++ src/resources/views/produtos/pesquisa.blade.php
@@ -178,9 +178,7 @@
     
 	<br>
 
-	<h1 class="font-weight-light text-center titulo mt-4 mb-0">você também pode gostar dessas artes</h1>
 
-  <hr class="mt-2 mb-5">
 
   <div class="container">
 <div class="row">
