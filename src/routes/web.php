039d407dab6748fda9b8bee2f7df8e0df7081b2d
Bruno César <>
2020-05-18 13:38:16 -0300

    Pradonização Model OK Correção
    Pradonização Model OK Correção

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'HomeController@index')->name("home");
+Route::get('/', 'Home\HomeController@index')->name("home");
 Route::redirect('/home', '/');
 
 Route::get("/login", function(){
