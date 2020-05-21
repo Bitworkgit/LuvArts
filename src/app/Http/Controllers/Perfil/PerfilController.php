c7d44e8c0457d3a0d0ba6b726a61b47cbb3b3d03
Thiago Piassi Bonfogo <thiagoprofissional29@gmail.com>
2020-05-21 14:38:01 -0300

    Update PerfilController.php

--- src/app/Http/Controllers/Perfil/PerfilController.php
+++ src/app/Http/Controllers/Perfil/PerfilController.php
@@ -27,7 +27,7 @@ class PerfilController extends Controller
             return redirect()->route("home");
         }
         else {
-            return usuario/perfil',compact('user','colecoes', 'see', 'verAdm','categorias','carrinho'));
+            return view('usuario/perfil',compact('user','colecoes', 'see', 'verAdm','categorias','carrinho'));
         }
     }  
 
