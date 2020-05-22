61ee13c21580c1e8b3e6623197191b50c2844343
Thiago Piassi Bonfogo <thiagoprofissional29@gmail.com>
2020-05-21 23:34:56 -0300

    correção de vulnerabilidades finalizada

--- src/webpack_js.config.js
+++ src/webpack_js.config.js
@@ -2,21 +2,23 @@ const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-   entry: './resources/js/app.js',
-   output:{
-       path: path.resolve(__dirname, 'public/js'),
-       filename: 'app.js'
-   },
-   module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      }
-    ]
+  entry: './resources/js/app.js',
+  output:{
+      path: path.resolve(__dirname, 'public/js'),
+      filename: 'app.js'
   },
-  plugins: [
-    new VueLoaderPlugin()
-  ],
-  mode: 'development'
+  module: {
+   rules: [
+     {
+       test: /\.vue$/,
+       loader: [
+           'vue-loader',
+       ]
+     }
+   ]
+ },
+ plugins: [
+   new VueLoaderPlugin()
+ ],
+ mode: 'development'
 };
