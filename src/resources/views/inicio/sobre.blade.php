8fb2d8c5af4c396fae7f59f1e5d4c82399685a88
Thiago Piassi Bonfogo <thiagoprofissional29@gmail.com>
2020-05-31 14:47:28 -0300

    Update sobre.blade.php

--- src/resources/views/inicio/sobre.blade.php
+++ src/resources/views/inicio/sobre.blade.php
@@ -170,22 +170,31 @@
 </script>
 
     <script src="{{ asset("js/app.js") }}"></script>
-
+    
     <div class="container">
-		<div class="about">
-			<div>
+      <br><br>
+      <center>
+        <div>
+          <img src="{{asset('/images/NAV.png')}}" class="img-fluid" style="width: 15%;">
+        </div>
+        <br><br>
 				<h1>Sobre nós</h1>
-			</div>
-		</div>
-		<div class="content">
-			<p><b>B</b>em vindos a nossa página, nós da LuvArt's agradecemos o seu acesso, somos uma equipe com o objetivo de poder
-                fazer a diferença ao mundo, através de nosso site você artista pode cadastrar seu trabalho e você consumidor poderá efetuar a compra de lindas artes, e ao mesmo tempo
-                ambos estarão ajudando instituições de caridade que sobrevivem apenas com doações. Assim nós da LuvArt's e você artista e consumidor estará ajudando a transformar o mundo
-                em um lugar melhor. :D </p>
-		</div>
-		</div>
-		<footer>Copyright &copy;LuvArt's 2020 </footer>
-
+		  </center>
+      <br>
+      <div class="content">
+        <p><b>B</b>em-vindos a nossa página nós da luvArtsagradecemos o seu acesso somos uma equipe com
+          objetivo de fazer a diferença ao mundo através de nosso site. Você artista pode cadastrar seu 
+          trabalho e você consumidor poderá efetuar a compra de lindas artes ao mesmo tempo ambos estarão 
+          ajudando instituições de caridade que sobrevivem apenas com doações, Deste modo a equipe da 
+          luvArt e você estarão ajudando a transformar o mundo em um lugar melhor. Esta plataforma tem
+          como objetivo auxiliar a instituições de caridade, o dinheiro de cada venda será dividido em
+          25% para o artista e o resto será destinado para entidades  de caridade e para a manutenção 
+          da plataforma. Buscamos também dar espaço para artistas amadores conquistar em seu publico... 
+          Acreditamos que todos merecem uma chance de mostrar seu potencial por isso na página principal 
+          selecionamos 9 artes aleatórias para serem colocadas na página principal sempre que entrar Será 
+          diferente.</p>
+		  </div>
+    </div>
 </body>
 
 </html>
